67bd4271e28a1e2cb9998a2420cd559910c6d724
takovoy <bazaster1@gmail.com>
2019-12-06 12:44:31 +0300

    remove webgl

--- frontend/src/app/main/index.module/index.screen.component/index.screen.component.ts
+++ frontend/src/app/main/index.module/index.screen.component/index.screen.component.ts
@@ -1,21 +1,8 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {ExcessorWebGL} from 'app/models/excessorWebGL.namespace';
-import {HttpClient} from '@angular/common/http';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-index-screen',
   templateUrl: './index.screen.component.html',
   styleUrls: ['./index.screen.component.scss']
 })
-export class IndexScreenComponent {
-  @ViewChild('canvasContainer') private canvasContainer: ElementRef;
-  private draw: ExcessorWebGL.IDraw = new ExcessorWebGL.Draw({
-    width: 500,
-    height: 500,
-    clearColor: [255, 255, 255, 1],
-  });
-
-  constructor (private http: HttpClient) {}
-
-  public ngOnInit() {}
-}
+export class IndexScreenComponent {}
